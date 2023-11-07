3925853cbb3c089f2ec2cc5b68d079c78da5f5c4
AlxndrKatz <137706323+AlxndrKatz@users.noreply.github.com>
2023-11-07 16:40:52 +0700

    Update Calculator.java

--- FireCalculator/src/main/java/Calculator.java
+++ FireCalculator/src/main/java/Calculator.java
@@ -1,36 +1,30 @@
 public class Calculator {
     public static double calculate(int calcInput) {
-        int year = calcInput - 2002;
+        int year = calcInput;
         double annualWithdrawalPerCent = 0.5;
+        double grade = 0.5;
         double deltaMoex;
         double deposit = 1;
         double withDrawal;
 
         while (deposit > 0.0) {
 
-            deposit = 100;
+            deposit = 1;
             withDrawal = annualWithdrawalPerCent * deposit;
             deposit = (withDrawal * 100) / annualWithdrawalPerCent;// См П.4 Задания
-            deposit -= withDrawal;
 
             for (int i = year + 1; i < 21; i++) {
+                double inflation = (1 + Constants.INFLATION_RATE[i] / 100);
 
-                if (annualWithdrawalPerCent * deposit / 100 * (1 + (Constants.INFLATION_RATE[i] / 100)) >
-                        (withDrawal * (1 + Constants.INFLATION_RATE[i] / 100))) {
-                    withDrawal = annualWithdrawalPerCent * deposit/ 100  * (1 + Constants.INFLATION_RATE[i] / 100);
-                } else {
-                    withDrawal = withDrawal * (1 + Constants.INFLATION_RATE[i] / 100);
-                }
-
+                withDrawal *= inflation;
                 deposit -= withDrawal;
                 deltaMoex = (Constants.MOEX_RATE[i] / Constants.MOEX_RATE[i - 1]);
                 deposit *= deltaMoex;
-
             }
             if (deposit > 0.0) {
-                annualWithdrawalPerCent += 0.5;
+                annualWithdrawalPerCent += grade;
             } else {
-                annualWithdrawalPerCent -= 0.5;
+                annualWithdrawalPerCent -= grade;
             }
         }
         return annualWithdrawalPerCent;
