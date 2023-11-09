a51701036211da6b118a7d2792b4d8f274ed3276
AlxndrKatz <137706323+AlxndrKatz@users.noreply.github.com>
2023-11-09 19:27:36 +0700

    Update Main.java

--- FireCalculator/src/main/java/Main.java
+++ FireCalculator/src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-        int calcInput = scanner.nextInt()-2002;
+        int calcInput = scanner.nextInt() - 2002;
 
         if (InputCheck.check(calcInput)) {
             double result = Calculator.calculate(calcInput);
