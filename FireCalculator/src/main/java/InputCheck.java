9ffb0f7ee910878b39d867e0f6c5c15a3ad1f318
AlxndrKatz <137706323+AlxndrKatz@users.noreply.github.com>
2023-11-09 19:27:00 +0700

    Update InputCheck.java

--- FireCalculator/src/main/java/InputCheck.java
+++ FireCalculator/src/main/java/InputCheck.java
@@ -1,9 +1,10 @@
-public class InputCheck{
+public class InputCheck {
     public static boolean check(int calcInput) throws Exception {
         checkInput(calcInput);
         return true;
     }
-    private static void checkInput (int calcInput) throws Exception{
+
+    private static void checkInput(int calcInput) throws Exception {
         if (calcInput > 19 || calcInput < 0) {
             throw new Exception("Exception 'Input is beyond range'");
         }
